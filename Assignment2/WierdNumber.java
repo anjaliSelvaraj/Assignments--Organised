2ea2e3d0461a7593afe14a5c2bebcf7c2a2d9b71
Anjali Selvaraj <anjali_selvaraj@yahoo.in>
2016-12-05 01:07:06 -0800

    Create WierdNumber.java

---
+++ Assignment2/WierdNumber.java
@@ -0,0 +1,13 @@
+public void weirdNumber(int n)
+{
+if(n%2 != 0)
+System.out.println("Weird");
+
+else
+if(n>=2 && n<=5)
+System.out.println("Not Weird");
+if(n>=6 && n<=20)
+System.out.println("Weird");
+if(n>20)
+System.out.println("not weird");
+}
